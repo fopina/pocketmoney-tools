89a494e8e07cb0efa57eb26073837a190d40ceeb
Filipe Pina <hzlu1ot0@duck.com>
2025-06-12 08:01:12 +0100

    x

---
+++ reconciliation/reconcile.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from functools import cached_property
+from pathlib import Path
+
+import classyclick
+
+
+@classyclick.command(context_settings={'show_default': True})
+class Push:
+    input: Path = classyclick.Argument()
+    pocketmoney: Path = classyclick.Option(default='pocketmoney_db_dump.json', help='Path to the converted JSON file')
+    account: str = classyclick.Option(help='PocketMoney account name. If not specified, it will in all accounts')
+    date_format: str = classyclick.Option('-f', default='%Y-%m-%d', help='Date format in CSV')
+    date_column: int = classyclick.Option('-d', default=0, help='0-index of the date column')
+    description_column: int = classyclick.Option('-c', default=1, help='0-index of the description column')
+    amount_column: int = classyclick.Option('-a', default=2, help='0-index of the amount column')
+
+    @cached_property
+    def data(self):
+        return json.loads(self.pocketmoney.read_text())
+
+    @cached_property
+    def accounts(self):
+        objs = {}
+        for obj in self.data['ICAccount']['data']:
+            if obj['ID'] in objs:
+                raise ValueError(f'Account {obj["ID"]} already exists')
+            objs[obj['ID']] = obj
+        return objs
+
+    @cached_property
+    def categories(self):
+        objs = {}
+        for obj in self.data['ICCategory']['data']:
+            if obj['ID'] in objs:
+                raise ValueError(f'Category {obj["ID"]} already exists')
+            objs[obj['ID']] = obj
+        return objs
+
+    @cached_property
+    def transactions(self):
+        objs = {}
+        for obj in self.data['ICTransaction']['data']:
+            if obj['ID'] in objs:
+                raise ValueError(f'Transaction {obj["ID"]} already exists')
+            obj['account'] = self.accounts[obj['account']]
+            objs[obj['ID']] = obj
+        return objs
+
+    @cached_property
+    def input_data(self):
+        transactions = []
+        with self.input.open('r') as f:
+            reader = csv.reader(f)
+            next(reader)  # Skip the header row
+            for row in reader:
+                transactions.append(
+                    Transaction(
+                        datetime.strptime(row[self.date_column].split(' ')[0], self.date_format).date(),
+                        row[self.description_column],
+                        float(row[self.amount_column]),
+                    )
+                )
+        return transactions
+
+    def generate_documents(self):
+        for trans in self.data['ICTransactionSplit']['data']:
+            # Create a document ID from the transaction's primary key
+
+            trans['amount'] = float(trans['amount'])
+            trans['transaction'] = self.transactions[trans['transaction']]
+            if self.account and trans['transaction'].get('account', {}).get('name', '').lower() != self.account.lower():
+                continue
+            if trans['category']:
+                trans['category'] = self.categories[trans['category']]
+
+            yield Transaction(
+                datetime.strptime(trans['transaction']['date'], '%Y-%m-%d').date(),
+                f'{trans["transaction"]["name"]} / {(trans["category"] or {}).get("name", "")}',
+                trans['amount'],
+            )
+
+    def __call__(self):
+        pocket_transactions: list[Transaction] = list(self.generate_documents())
+        csv_transactions: list[Transaction] = self.input_data
+        # make sure both are sorted in descendent order to optimize search
+        pocket_transactions.sort(reverse=True)
+        csv_transactions.sort(reverse=True)
+
+        for ct in csv_transactions:
+            for pt in pocket_transactions:
+                if pt.matched:
+                    continue
+                if abs(ct.date - pt.date) > timedelta(days=5):
+                    # don't match with more than 5 days apart
+                    continue
+                if ct.amount == pt.amount:
+                    print('MATCH', ct, pt)
+
+
+@dataclass(order=True)
+class Transaction:
+    date: datetime.date
+    description: str
+    amount: float
+    matched: 'Transaction' = None
+
+
+if __name__ == '__main__':
+    Push.click()
